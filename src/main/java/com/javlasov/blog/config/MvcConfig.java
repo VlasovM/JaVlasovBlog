07727fa9f40cf0c1cfddee6488c7fddd4b937d17
Vlasov M. (laprop) <m.a.vlasov97@gmial.com>
2022-07-26 08:04:33 +0300

    Tried to fixed bug with js

--- src/main/java/com/javlasov/blog/config/MvcConfig.java
+++ src/main/java/com/javlasov/blog/config/MvcConfig.java
@@ -11,8 +11,14 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/upload/**")
                 .addResourceLocations("file:/upload/");
-        registry.addResourceHandler("/templates/**")
-                .addResourceLocations("classpath:/templates/");
+        registry.addResourceHandler("/js/**")
+                .addResourceLocations("classpath:/static/js");
+        registry.addResourceHandler("/css/**")
+                .addResourceLocations("classpath:/static/css");
+        registry.addResourceHandler("/img/**")
+                .addResourceLocations("classpath:/static/img");
+        registry.addResourceHandler("/fonts/**")
+                .addResourceLocations("classpath:/static/fonts");
     }
 
 }
